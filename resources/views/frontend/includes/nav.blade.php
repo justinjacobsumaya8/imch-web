558ef810b78a1aad2517cecfa7f9fea735c3704c
Lourdes Sumaya <lourdessumaya@Lourdess-MacBook-Air.local>
2021-05-21 17:30:05 +0800

    changes

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -46,9 +46,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">Our Doctors</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Blogs</a>
-                    </li>
                 </ul>
             </div>
             <div class="">
