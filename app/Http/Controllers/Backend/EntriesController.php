487d22fbd570f8c912a54ab7aa0bb1f5cb9221e0
Lourdes Sumaya <lourdessumaya@Lourdess-MacBook-Air.local>
2021-05-23 19:47:19 +0800

    change ifnull to nullif postgre issue

--- app/Http/Controllers/Backend/EntriesController.php
+++ app/Http/Controllers/Backend/EntriesController.php
@@ -34,7 +34,8 @@ class EntriesController extends Controller
                                 AND e.is_resched = false 
                                 AND e.is_approved = true
                                 GROUP BY schedule_id) e "),"e.schedule_id","schedules.id")
-                            ->select('schedules.name', 'schedules.limit', DB::raw('IFNULL(e.schedule_count, 0) as schedule_count'), DB::raw('(schedules.limit - IFNULL(e.schedule_count, 0)) as schedule_availability'))
+                            // ->select('schedules.name', 'schedules.limit', DB::raw('IFNULL(e.schedule_count, 0) as schedule_count'), DB::raw('(schedules.limit - IFNULL(e.schedule_count, 0)) as schedule_availability'))
+                            ->select('schedules.name', 'schedules.limit', DB::raw('NULLIF(e.schedule_count, 0) as schedule_count'), DB::raw('(schedules.limit - NULLIF(e.schedule_count, 0)) as schedule_availability'))
                             ->where('schedules.is_deleted', false)
                             ->find($entry_schedule->schedule_id);
 
@@ -58,7 +59,7 @@ class EntriesController extends Controller
                                 AND e.is_resched = false 
                                 AND e.is_approved = true
                                 GROUP BY schedule_id) e "),"e.schedule_id","schedules.id")
-                            ->select('schedules.name', 'schedules.limit', DB::raw('IFNULL(e.schedule_count, 0) as schedule_count'), DB::raw('(schedules.limit - IFNULL(e.schedule_count, 0)) as schedule_availability'))
+                            ->select('schedules.name', 'schedules.limit', DB::raw('NULLIF(e.schedule_count, 0) as schedule_count'), DB::raw('(schedules.limit - NULLIF(e.schedule_count, 0)) as schedule_availability'))
                             ->where('schedules.is_deleted', false)
                             ->find($entry->schedule_id);
 
